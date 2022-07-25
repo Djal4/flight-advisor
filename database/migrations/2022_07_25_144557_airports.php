f3e92d140b7ab7aec201d63d8ce529988ec0c62d
Djal4 <aleksandar.djalovic@quantoxtechnology.com>
2022-07-25 17:14:38 +0200

    initial commit

---
+++ database/migrations/2022_07_25_144557_airports.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable('airports')){
+            Schema::create('airports',function(Blueprint $table){
+                $table->id();
+                $table->string('name');
+                $table->foreignId('city_id')->constrained('cities');
+                $table->char('iata',3)->unique()->nullable();
+                $table->char('icao',4)->unique()->nullable();
+                $table->double('latitude',20,15);
+                $table->double('longitude',20,15);
+                $table->smallInteger('altitude',false,true);
+                $table->smallInteger('timezone');
+                $table->foreignId('dst_id')->constrained('dst');
+                $table->foreignId('timezone_id')->constrained('timezones');
+                $table->foreignId('type_id')->constrained('types');
+                $table->foreignId('source_id')->constrained('sources');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('airports');
+    }
+};
---
+++ database/migrations/2022_07_25_145521_routes.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable('routes')){
+            Schema::create('routes',function(Blueprint $table){
+                $table->id();
+                $table->foreignId('airline_id')->constrained('airlines');
+                $table->foreignId('source_airport_id')->constrained('airports');
+                $table->foreignId('destionation_airport_id')->constrained('airports');
+                $table->char('codeshare',1)->default('');
+                $table->smallInteger('stops',false,true);
+                $table->foreignId('equipment_id')->constrained('equipment');
+                $table->double('price',8,2);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routes');
+    }
+};
