5245057e8eadaedd27da69a31d2a424a0b3de6ef
Djal4 <aleksandar.djalovic@quantoxtechnology.com>
2022-07-30 15:46:00 +0200

    City endpoint.

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\City;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\User;
 class CityController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index($number=0)
-    {
+    {      
+        $this->authorize('viewAny',City::class);
         $query=DB::table('comments')
             ->join('cities','comments.city_id','=','cities.id')
             ->join('users','comments.user_id','=','users.id')
@@ -47,13 +49,14 @@ class CityController extends Controller
      */
     public function store(Request $request)
     {
-        return response()->json([
+        $this->authorize('create',City::class);
+        return response()->json(
             City::create([
-                "name"=>$request->name,
-                "country_id"=>$request->country_id,
-                "description"=>$request->description
+                'name'=>$request->name,
+                'country_id'=>$request->country_id,
+                'description'=>$request->description
             ])
-        ]);
+        );
     }
 
     /**
@@ -64,6 +67,7 @@ class CityController extends Controller
      */
     public function show($name,$number=0)
     {
+        $this->authorize('view',City::class);
         $query=DB::table('comments')
             ->join('cities','comments.city_id','=','cities.id')
             ->join('users','comments.user_id','=','users.id')
@@ -75,42 +79,8 @@ class CityController extends Controller
                 
         }
 
-        return response()->json([
+        return response()->json(
             $query->get()
-        ]);
-    }
-    
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        );
     }
 }
